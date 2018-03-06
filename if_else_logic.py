2d63b120610344d7569822be136f804a0690bdb1
JCruz6725 <35982889+JCruz6725@users.noreply.github.com>
2018-03-05 19:56:53 -0600

    Add files via upload

---
+++ if_else_logic.py
@@ -0,0 +1,59 @@
+#John Cruz
+#2/22/2018
+#Gym Exersize based on height and wieght
+import time as t
+
+'''
+def height_to_wieght():
+'''
+
+Power_on = 'Y'
+while Power_on == 'Y'): #Power on or open app
+
+	feet = int (input('how tall are you in feet '))
+	inches = int (input('how tall are you in inches '))
+	weight = int(input ('how much do you weight '))
+
+	height = feet + inches/12
+
+	print ('you are %d\'%d\" '%(feet, inches))
+	print (height)
+	print (weight)
+
+	#for 6+feet and 200-250lbs
+
+	Suggestion1 = '1. Cycle 30 min, Tredmill 20 min, lift up to 50lbs for 10min\n'
+
+	#for 5-6feet and 175-200lbs
+	Suggestion2 = '2. Cycle 20 min, Tredmill 20 min, lift up to 40lbs for 10min\n'
+
+	#for less than 5feet and 100-150lbs
+	Suggestion3 = '3. Cycle 20 min, Tredmill 10 min, lift up to 20lbs for 10min\n'
+
+	#for any thing that does not fall under any cat.
+	GoToDr = '4. You should see a doctor.\n'
+
+	if ((height >= 6) and (200 <= weight <=250)):
+		print(Suggestion1)
+		t.sleep(.5)
+
+	elif((5 <= height < 6) and (175 <= weight <= 200)):
+		print(weight)
+		print(Suggestion2)
+		t.sleep(.5)
+
+	elif((height < 5) and (100 <= weight <= 150)):
+		print(Suggestion3)
+		t.sleep(.5)
+	else:
+		print (GoToDr)
+		t.sleep(.5)
+	
+	Power_on = str(input ('Continue? Y/N?'))
+
+
+
+
+
+
+
